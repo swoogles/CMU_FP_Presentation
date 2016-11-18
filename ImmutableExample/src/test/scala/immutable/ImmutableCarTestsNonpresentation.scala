10267501cc6ede517bbec18593711948fbf0e84f
bfrasure <bill@solutionreach.com>
2016-11-18 14:36:12 -0700

    +Separate test classes for non-shown cases  #time 10m

---
+++ ImmutableExample/src/test/scala/immutable/ImmutableCarTestsNonpresentation.scala
@@ -0,0 +1,122 @@
+package immutable
+
+import functionalpresentation.{Home, Restaurant, School}
+import org.scalatest.FlatSpec
+
+import pprint.Config
+import scala.util.Try
+
+class ImmutableCarTestsNonpresentation extends FlatSpec {
+  implicit val pprintConfig = Config()
+
+  val SAM = Person("Sam", Home)
+  val JOE = Person("Joe", Home)
+  val CAR = Car(100, Home)
+  val SCENE = Scene(JOE, SAM, CAR)
+
+  "Driving" should "not be possible if you aren't with the car" in {
+    val scene = Scene(JOE, SAM, CAR.copy(location=Restaurant))
+    val sceneResult = Scenarios.processScenes(
+      scene,
+      SceneUpdate(joe=Home, sam=Restaurant)
+    )
+    assert(sceneResult.isFailure)
+  }
+
+  "Driving" should "fail if both people want to travel to different places." in {
+    val sceneResult = Scenarios.processScenes(
+      SCENE,
+      SceneUpdate(joe=School, sam=Restaurant)
+    )
+    assert(sceneResult.isFailure)
+
+  }
+
+//  "Driving" should "fail if joe comes to sam, and then sam's movement is applied" in {
+//    println("test of interest")
+//        val startScene = Scene(JOE.copy(location = School), SAM, CAR.copy(location=School))
+//    val sceneResult = Scenarios.processScenesTyped(
+//      startScene,
+//      List(
+//        SceneUpdate(joe=Home, sam=Restaurant)
+//      )
+//    )
+//    println("done with test body")
+//    assert(sceneResult.isFailure)
+//
+//  }
+
+  "Driving" should "cease when you run out of gas" in {
+    val sceneResult = Scenarios.processScenesCumulative(
+      SCENE,
+      SceneUpdate(joe=Home, sam=Restaurant),
+      SceneUpdate(joe=Home, sam=School),
+      SceneUpdate(joe=Home, sam=Home),
+      SceneUpdate(joe=Home, sam=School),
+      SceneUpdate(joe=Home, sam=Home),
+      SceneUpdate(joe=School, sam=Home)
+    )
+
+    pprint.pprintln(sceneResult)
+    assert(sceneResult.last.isFailure, true)
+  }
+
+
+  "Driving" should "chain" in {
+
+    val sceneResult = SCENE
+      .update(SceneUpdate(joe=Home, sam=School))
+      .update(SceneUpdate(joe=Home, sam=Home))
+      .update(SceneUpdate(joe=Home, sam=School))
+      .update(SceneUpdate(joe=Home, sam=Home))
+      .update(SceneUpdate(joe=School, sam=Home))
+
+    println("Chained result:")
+    pprint.pprintln(sceneResult)
+//    assert(sceneResult.last.isFailure, true)
+  }
+
+//  "Driving" should "handle failures and apply valid movements as possible" in {
+//    val startScene = Scene(SAM, JOE, CAR)
+//    val sceneResult = Scenarios.processScenesFaultTolerantTyped(
+//      startScene,
+//      SceneUpdate(joe=School, sam=Restaurant), // Fail - Diverging locations
+//      SceneUpdate(joe=Home, sam=School),
+//      SceneUpdate(joe=Home, sam=Home),
+//      SceneUpdate(joe=School, sam=Restaurant), // Fail - Diverging locations
+//      SceneUpdate(joe=Home, sam=School),
+//      SceneUpdate(joe=Home, sam=Home),
+//      SceneUpdate(joe=Home, sam=School),
+//      SceneUpdate(joe=Home, sam=Home),
+//      SceneUpdate(joe=School, sam=Home)
+//    )
+//
+//    pprint.pprintln(sceneResult)
+//  }
+
+  // TODO put in separate test since it doesn't involve scene updating.
+//  "A car" should "should always have fuel after fuel check." in {
+//    assert(CAR.fuel == 100)
+//    TravelFunctions.drive(SAM, CAR, Restaurant) // Does nothing, since the result is ignored
+//    assert(CAR.fuel == 100)
+//  }
+
+    "Driving" should "cease when you run out of gas, but preserve last state" in {
+      val sceneResult = Scenarios.processScenesKeepLastGoodStateScene(
+        SCENE,
+        SceneUpdate(joe=Home, sam=Restaurant),
+        SceneUpdate(joe=Home, sam=School),
+        SceneUpdate(joe=Home, sam=Home),
+        SceneUpdate(joe=Home, sam=School),
+        SceneUpdate(joe=Home, sam=Home),
+        SceneUpdate(joe=School, sam=Home)
+      )
+
+      pprint.pprintln(sceneResult)
+      sceneResult match {
+        case Right(nonBrokenScene) => fail()
+        case Left(failedSceneWithState) => println("Failed scene: " + failedSceneWithState)
+      }
+    }
+
+}
---
+++ ImmutableExample/src/test/scala/mutable/MutableCarTestsWithDebugging.scala
@@ -0,0 +1,47 @@
+package mutable
+
+import functionalpresentation.{Home, Restaurant, School}
+import org.scalatest.{Assertion, FlatSpec}
+
+class MutableCarTestsWithDebugging extends FlatSpec {
+
+  var sam = Person("Sam", Home)
+  var joe = Person("Joe", Home)
+  var car = Car(100, Home)
+
+  "Driving" should "cease when you run out of gas" in {
+      if (car.fuel < 100)
+        joe.fill(car)
+
+      if (car.fuel == 100) {
+        println("1")
+        println("car.fuel: " + car.fuel)
+        sam.drive(car, Restaurant)
+        println("2")
+        println("car.fuel: " + car.fuel)
+        sam.drive(car, School)
+        println("3")
+        println("car.fuel: " + car.fuel)
+        sam.drive(car, Restaurant)
+        println("3")
+        println("car.fuel: " + car.fuel)
+        sam.drive(car, Home) // Error: Out of gas!
+        println("4")
+        println("car.fuel: " + car.fuel)
+        sam.clean(car)
+        println("Car's clean. We've got 20 units of fuel left. Let's drive!")
+        joe.drive(car, Home) // Error: Out of gas!
+        println("Driving completed successfully!")
+      }
+  }
+
+  "Car and driver" should "end in different locations to demonstrate subtle bug" in {
+    val startLocation = joe.location
+    val expectedFailure: Assertion = assertThrows[Exception] {
+      car.fuel = 0
+      joe.drive(car, School) // Error: Out of gas!
+    }
+    assert(joe.location == startLocation, "Joe should not have moved since the car was out of gas.")
+  }
+
+}
